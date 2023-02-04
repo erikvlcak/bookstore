ef92186c09e2d1abbd0f07b9d902995e484e2e3a
erikvlcak <erikvlcak1@gmail.com>
2023-02-04 22:40:59 +0100

    Add script for book purchase

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ video.load();
 //Add coins to wallet and add that value to coins property for further use.
 let coinsField = document.querySelector('.add-money');
 let coinsCount = document.querySelector('.wallet-sum');
-let coins = null;
+let wallet = null;
 
 coinsField.addEventListener('click', (e) => {
     if (e.target.className == 'button') {
@@ -23,8 +23,8 @@ coinsField.addEventListener('click', (e) => {
             coinsField.querySelector('input').style.backgroundColor = 'grey';
             coinsField.querySelector('input').disabled = 'true';
             coinsField.querySelector('label').textContent = 'Coins are in your wallet.'
-            coins = value;
-            console.log(coins);
+            wallet = value;
+            console.log(wallet);
         } else if (isFinite(value) && value < 0) {
             coinsField.querySelector('input').value = '';
             alert('C\'mon, you can\'t have negative coins.');
@@ -36,13 +36,40 @@ coinsField.addEventListener('click', (e) => {
     }
 });
 
+
+//Remove book from the list if you have enough Coins. Subtract price from your wallet.
 let sellList = document.querySelectorAll('.sellBooksList');
 
 sellList.forEach((book) => {
     book.addEventListener('click', (e) => {
         if (e.target.tagName == 'BUTTON') {
-            e.target.parentElement.parentElement.removeChild(e.target.parentElement);
+            let bookPrice = parseInt(e.target.previousElementSibling.querySelector('.coinsPrice').textContent);
+            console.log(`bookPrice is ${bookPrice}`)
+            if (!wallet) {
+                alert('You have no money in your wallet. Add some!');
+            } else if (parseInt(bookPrice) <= parseInt(wallet)) {
+                alert(`
+                Spellbook
+                
+                "${e.target.previousElementSibling.querySelector('.bookName').textContent}"
+
+                have been purchased for ${e.target.previousElementSibling.querySelector('.coinsPrice').textContent} Coins.
+                
+                Enjoy your reading!`);
+                e.target.parentElement.parentElement.removeChild(e.target.parentElement);
+                coinsCount.textContent = wallet - bookPrice;
+                wallet = parseInt(coinsCount.textContent);
+            } else {
+                alert(`Sorry, you are ${bookPrice - wallet} Coins short for this one.`)
+            }
         }
+
+        if (document.querySelectorAll('.sellBooksList li').length == 0) {
+            document.querySelector('.empty').style.display = 'block';
+        }
+
+
+
     })
 })
 
